195a2e2450e6c7e0bf87cdb4ff221a8c72ac7c2a
Horieh Ghiasidalalian <horiehghiasi@gmail.com>
2023-12-19 10:58:42 +0100

    Add project files.

---
+++ BethanysPieShopHRM.App/Pages/EmployeeOverview.razer.cs
@@ -0,0 +1,18 @@
+﻿using BethanysPieShopHRM.App.Models;
+using BethanysPieShopHRM.Shared.Domain;
+
+
+namespace BethanysPieShopHRM.App.Pages
+{
+    public partial class EmployeeOverview //we can have the same class with the same name if we put partial 
+    {
+        public List<Employee> Employees { get; set; } = default!;
+
+
+        protected override void OnInitialized()
+        {
+            Employees = MockDataService.Employees;
+        }
+
+    }
+}
